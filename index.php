e48dd5f15ad29e572408c0c7fec600eeb0838c33
GoodestBoi! <83266402+Ezyrus@users.noreply.github.com>
2023-05-30 22:28:08 +0800

    config

--- index.php
+++ index.php
@@ -12,35 +12,37 @@ include 'dbConnection.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pharmacy Management System</title>
 
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- AdminLTE Theme style -->
     <link rel="stylesheet" href="dist/css/adminlte.min.css">
     <!-- jQuery -->
     <script src="plugins/jquery/jquery.min.js"></script>
-      <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <!-- icheck bootstrap -->
+    <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <!-- AdminLTE App -->
     <script src="dist/js/adminlte.min.js"></script>
-      <!-- Font Awesome -->
-  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
 </head>
 
 <body class="hold-transition login-page">
 
     <div class="login-box">
-        <!-- /.login-logo -->
+
         <div class="card card-outline card-primary">
             <div class="card-header text-center">
-                <h1>LOG-IN</h1>
+                <h2><i class="fas fa-solid fa-syringe fa-xl"></i> Pharmacy Management System</h2>
             </div>
             <div class="card-body">
                 <p class="login-box-msg">Sign in to start your session</p>
 
                 <form action="../../index3.html" method="post">
                     <div class="input-group mb-3">
-                        <input type="email" class="form-control" placeholder="Email">
+                        <input type="text" class="form-control" placeholder="Username">
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-envelope"></span>
@@ -56,7 +58,7 @@ include 'dbConnection.php';
                         </div>
                     </div>
                     <div class="row mb-2">
-                 
+
                         <div class="col">
                             <button type="submit" class="btn btn-primary btn-block">Sign In</button>
                         </div>
@@ -64,11 +66,8 @@ include 'dbConnection.php';
 
                 </form>
 
-                <p class="mb-1">
-                    <a href="forgot-password.html">I forgot my password</a>
-                </p>
                 <p class="mb-0">
-                    <a href="register.html" class="text-center">Register a new membership</a>
+                    <a href="register.php" class="text-center text-decoration-none">Register a new membership</a>
                 </p>
             </div>
             <!-- /.card-body -->
